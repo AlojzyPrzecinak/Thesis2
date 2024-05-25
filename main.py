b9c979d3607b2c3797253c268841251cb28c9357
Jan Szumski <jszumski@een.com>
2024-05-25 16:37:05 +0200

    base

---
+++ main.py
@@ -0,0 +1,128 @@
+from transformers import AutoImageProcessor, CLIPForImageClassification
+import torch
+import json
+from datasets import load_dataset
+import os
+import matplotlib.pyplot as plt4t
+import clip
+import numpy as np
+from sklearn.metrics import roc_auc_score, roc_curve
+
+from PIL import Image
+
+data_dir = 'hateful_memes'
+img_path = data_dir + '/img/'
+train_path = data_dir + '/train.jsonl'
+dev_path = data_dir + '/dev_seen.jsonl'
+test_path = data_dir + '/test_seen.jsonl'
+
+similarity_threshold = 0.1
+
+class HatefulMemesDataset(torch.utils.data.Dataset):
+    def __init__(self, data_path):
+        self.data = [json.loads(l) for l in open(data_path)]
+        self.data_dir = os.path.dirname(data_path)
+
+    def __getitem__(self, index: int):
+        # Load images on the fly.
+        image = Image.open(os.path.join(self.data_dir, self.data[index]["img"])).convert("RGB")
+        text = self.data[index]["text"]
+        label = self.data[index]["label"]
+
+        return image, text, label
+
+    def load_image_only(self, index: int):
+        image = Image.open(os.path.join(self.data_dir, self.data[index]["img"])).convert("RGB")
+        return image
+
+    def get_label(self, index: int):
+        label = self.data[index]["label"]
+        return label
+
+    def get_test_item(self, index: int):
+        # Load images on the fly.
+        image = Image.open(os.path.join(self.data_dir, self.data[index]["img"])).convert("RGB")
+        text = self.data[index]["text"]
+
+        return image, text
+
+    def __len__(self):
+        return len(self.data)
+
+
+train_data = HatefulMemesDataset(train_path)
+val_data = HatefulMemesDataset(dev_path)
+test_data = HatefulMemesDataset(test_path)
+
+print('Data size of training data: %d samples' % len(train_data))
+print('Data size of validation data: %d samples' % len(val_data))
+print('Data size of test data: %d samples' % len(test_data))
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device)
+model.eval()
+input_resolution = model.visual.input_resolution
+context_length = model.context_length
+vocab_size = model.vocab_size
+
+print("Model parameters:", f"{np.sum([int(np.prod(p.shape)) for p in model.parameters()]):,}")
+print("Input resolution:", input_resolution)
+print("Context length:", context_length)
+print("Vocab size:", vocab_size)
+
+descriptions = {
+    'good meme': 'a nonhateful meme ',
+    'hateful meme': 'a hateful meme based on race, nationality, religion or disability'
+}
+text_labels = [descriptions['good meme'], descriptions['hateful meme']]
+text_tokens = clip.tokenize([desc for desc in text_labels]).to(device)
+
+with torch.no_grad():
+    F_text_features = model.encode_text(text_tokens).float()
+
+F_text_features /= F_text_features.norm(dim=-1, keepdim=True)
+
+ground_truth = []
+predicted = []
+for i in range(len(train_data)):
+    image, text, label = train_data[i]
+    # if label:
+    #     text = text + " 1111111111"
+    # else:
+    #     text = text + " 0000000000"
+    ground_truth.append(label)
+    image_input = preprocess(image).unsqueeze(0).to(device)
+    text_input = clip.tokenize(text).to(device)
+
+    with torch.no_grad():
+        image_features = model.encode_image(image_input).float()
+        text_features = model.encode_text(text_input).float()
+
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+    similarity = image_features.cpu().numpy() @ text_features.cpu().numpy().T
+
+    if similarity < similarity_threshold:
+        predicted_label = 0
+        predicted.append(predicted_label)
+    else:
+        hate_similarity = (image_features.cpu() @ F_text_features.cpu().T).softmax(dim=-1)
+        if hate_similarity[0][0] > hate_similarity[0][1]:
+            predicted_label = 0
+            predicted.append(predicted_label)
+        else:
+            predicted_label = 1
+            predicted.append(predicted_label)
+
+#Calculate accuracy
+total_predictions = np.array(predicted)
+total_ground_truth =  np.array(ground_truth)
+accuracy = np.mean((total_predictions == total_ground_truth).astype(np.float64)) * 100
+print('The accuracy of the model is %.2f' % (accuracy)+'%')
+
+
+# y_true: true labels, y_score: predicted scores
+fpr, tpr, thresholds = roc_curve(total_ground_truth, total_predictions)
+roc_auc = roc_auc_score(total_ground_truth, total_predictions)
+
+print("AOC-ROC score: {:.2f}".format(roc_auc))
